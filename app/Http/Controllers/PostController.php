1b8e6b8712162a12ea66ce96f6bab2d43bb3cfd0
Vladyslav Prykhodko <weijin@MacBook-Pro-Vladyslav.local>
2017-03-04 21:47:39 +0200

    POST STORE FIX

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,6 +35,9 @@ class PostController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
+        return response()->json([
+            'data' => $data
+        ]);
         DB::beginTransaction();
         try {
             if (!isset($data['posts'])) {
